ff0878f67b5e8c298c00a74faac241fd6393c9fc
mortiz510 <maurortiz510@yahoo.com>
2017-06-14 15:30:48 -0300

    Add files via upload

---
+++ MI 5.cpp
@@ -0,0 +1,16 @@
+/*
+Mauro Ariel Ortiz
+14/06/2017
+MI 5
+*/
+#include <iostream>
+int main () {
+	int a;
+	int b;
+	std:: cin>>a>>b;
+	if (a>b)
+	    std:: cout<<"El mayor es "<<a<<"\n";
+    else {
+    	std:: cout<<"El mayor es "<<b<<"\n";
+	}
+}
