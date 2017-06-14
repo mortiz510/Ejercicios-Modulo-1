a006d2483b390ac73bd4051396c73c3cb4671602
mortiz510 <maurortiz510@yahoo.com>
2017-06-14 15:11:04 -0300

    Add files via upload

---
+++ Actividad 4.cpp
@@ -0,0 +1,16 @@
+/* Mauro Ariel Ortiz
+14/6/2017
+Ejercicio 4 
+*/
+#include <iostream>
+int main () {
+	int a;
+	double q,r,s;
+	std:: cin>>a;
+	q=a;
+	r=a%5;
+	s=q/7;
+	std:: cout<<"Su quinta parte es "<<q/5<<"\n";
+	std:: cout<<"El resto dividirlo por 5 es "<<r<<"\n";
+	std:: cout<<"La septima parte del primer resultado es "<<s<<"\n";
+}
