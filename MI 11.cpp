32008eb5b71bb7472f895443922405ad756ff0f7
mortiz510 <maurortiz510@yahoo.com>
2017-06-14 23:01:11 -0300

    Add files via upload

---
+++ MI 11.cpp
@@ -0,0 +1,19 @@
+/*
+Mauro Ariel Ortiz
+14/06/2017
+MI 11
+*/
+#include <iostream>
+int main () {
+	unsigned a;
+	std:: cin>>a;
+	if (a<=12)
+	    std:: cout<<"menor";
+	    else if (a>=13 and a<=18)
+	    	std:: cout<<"cadete";
+	    	else if (a>18 and a<26)
+	    		std:: cout<<"juvenil";
+	    		else 
+	    			std:: cout<<"mayor";
+				
+}
